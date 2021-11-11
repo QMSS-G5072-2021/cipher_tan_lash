8cfadd7a1a15e8f074513e04301b5be7d9a2e97f
Lash Tan <lashjtan@gmail.com>
2021-11-10 22:29:47 -0500

    Upload jupyter notebook

---
+++ cipher_ljt2138/jupyter_execute/example.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Example usage
+# 
+# To use `cipher_ljt2138` in a project:
+
+# In[1]:
+
+
+import cipher_ljt2138
+
+print(cipher_ljt2138.__version__)
+
